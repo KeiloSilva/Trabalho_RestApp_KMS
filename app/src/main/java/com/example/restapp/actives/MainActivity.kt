3368ba0066d54bb31acb942e70ff43b491c24514
KeiloSilva <keilomarcio062014@gmail.com>
2019-09-19 17:22:10 -0300

    Primeira versão do trabalho do Edson Angoti

---
+++ app/src/main/java/com/example/restapp/actives/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.restapp.actives
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.restapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
